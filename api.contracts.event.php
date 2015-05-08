ea9667e8ce0f7d9a35a0a499d3e8d7ac16392f06
Zhu Liangxiong <lxzhu@outlook.com>
2015-05-08 17:20:39 +0800

    Update api.contracts.event.php

--- api.contracts.event.php
+++ api.contracts.event.php
@@ -1,5 +1,46 @@
-####
+<?php
+#注册活动
 class EventRegisterRequest{}
 class EventRegisterResponse{}
+
+#修改活动信息
 class EventUpdateRequest{}
 class EventUpdateResponse{}
+
+#关注活动
+class EventWatchRequest{}
+class EventWatchResponse{}
+
+#不在关注活动
+class EventUnwatchRequest{}
+class EventUnwatchResponse{}
+
+#参加活动
+class EventAttendRequest{}
+class EventAttendResponse{}
+
+#退出活动
+class EventQuitRequest{}
+class EventQuitResponse{}
+
+#搜索活动
+class EventSearchRequest{}
+class EventSearchResponse{}
+
+#发布活动
+class EventPublishRequest{}
+class EventPublishResponse{}
+
+#开始活动
+class EventStartRequest{}
+class EventStartResponse{}
+
+#结束活动
+class EventFinishRequest{}
+class EventFinishResponse{}
+
+#取消活动
+class EventCancelRequest{}
+class EventCancelResponse{}
+
+?>
