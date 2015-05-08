55a9318928f6bf114e0592a177e56d1b48967698
Zhu Liangxiong <lxzhu@outlook.com>
2015-05-08 17:33:07 +0800

    Create api.contracts.sitemail.php

---
+++ api.contracts.sitemail.php
@@ -0,0 +1,22 @@
+<?php
+#保存为草稿
+class SiteMailSaveRequest{}
+class SiteMailSaveResponse{}
+
+#发送
+class SiteMailSendRequest{}
+class SiteMailSendResponse{}
+
+#查看消息
+class SiteMailSearchRequest{}
+class SiteMailSearchResponse{}
+
+#设置状态，设置为已读,etc
+class SiteMailSetStatusRequest{}
+class SiteMailSetStatusResponse{}
+
+#删除消息
+class SiteMailDeleteRequest{}
+class SiteMailDeleteResponse{}
+
+?>
