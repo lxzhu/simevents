fd7b0b4f78712a985a1bfb1898f8e8028cb672da
Zhu Liangxiong <lxzhu@outlook.com>
2015-05-08 17:13:09 +0800

    Create api.contracts.user.php

---
+++ api.contracts.user.php
@@ -0,0 +1,34 @@
+<?php
+#
+# we do not use these contacts but we require these classes to be updated to 
+# make sure we can generate correct api document.
+# 
+####USER####
+class UserRegisterRequest{
+  
+}
+class UserRegisterResponse{
+}
+class UserLoginRequest{
+}
+class UserLoginResponse{
+}
+class UserLogoutRequest{
+}
+class UserLogoutResponse{
+}
+class UserUpdateProfileRequest{
+}
+class UserUpdateProfileResponse{
+}
+class UserContactVerifyRequest{
+}
+class UserContactVerifyResponse{
+}
+class UserPasswordResetRequest{
+}
+class UserPasswordResetResponse{
+}
+
+
+?>
