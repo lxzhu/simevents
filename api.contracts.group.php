794e9ffb13363e6d8ab85d47665b9f5fcaa79a55
Zhu Liangxiong <lxzhu@outlook.com>
2015-05-08 17:13:31 +0800

    Create api.contracts.group.php

---
+++ api.contracts.group.php
@@ -0,0 +1,17 @@
+####GROUP
+class GroupRegisterRequest{}
+class GroupRegisterResponse{}
+class GroupUpdateRequest{}
+class GroupUpdateResponse{}
+class GroupSearchGroupRequest{}
+class GroupSearchGroupResponse{}
+class GroupCloseRequest{}
+class GroupCloseResponse{}
+
+class GroupAddMemberRequest{}
+class GroupAddMemberResponse{}
+class GroupRemoveMemberRequest{}
+class GroupRemoveMemberResponse{}
+class GroupSearchGroupMemberRequest{}
+class GroupSearchGroupMemberResponse{}
+
